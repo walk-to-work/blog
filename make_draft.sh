577f6c433c826d7395e475f46ad868db6c6667ef
walk_to_work <fallere15423@gmail.com>
2020-04-17 00:15:29 +0900

    fix make_draft.sh

--- make_draft.sh
+++ make_draft.sh
@@ -4,6 +4,6 @@ echo "make draft : ${title}"
 touch ./content/posts/${title}.md
 image_dir_name=`date "+%Y%m%d"`
 mkdir ./static/${title}
-today=`date "+%Y%m%d-%H%M%S"`
+today=`date "+%Y-%m-%d"`
 echo ${today}
-echo "+++\ntitle = \"${title}\"\ntags = [\"\"]\ndate = \"${today}\"\n+++\n\n![image_name](/${title}/image_name.png)" >> ./content/posts/${title}
+echo "+++\ntitle = \"${title}\"\ntags = [\"\"]\ndate = \"${today}\"\n+++\n\n![image_name](/${title}/image_name.png)" >> ./content/posts/${title}.md
