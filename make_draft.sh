fd614906638ad4c2477f5c0ab94edbeb21c82893
walk_to_work <fallere15423@gmail.com>
2021-02-11 23:14:03 +0900

    [fix] make_draft.sh generation of iamge path

--- make_draft.sh
+++ make_draft.sh
@@ -3,7 +3,7 @@ title=$1
 echo "make draft : ${title}"
 touch ./content/posts/${title}.md
 image_dir_name=`date "+%Y%m%d"`
-mkdir ./static/${title}
+mkdir ./static/images/${title}
 today=`date "+%Y-%m-%d"`
 echo ${today}
-echo "+++\ntitle = \"${title}\"\ntags = [\"\"]\ndate = \"${today}\"\n+++\n\n![image_name](/${title}/image_name.png)" >> ./content/posts/${title}.md
+echo "+++\ntitle = \"${title}\"\ntags = [\"\"]\ndate = \"${today}\"\n+++\n\n![image_name](/images/${title}/image_name.png)" >> ./content/posts/${title}.md
